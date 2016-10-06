c0fd418b003c6190b210f2e506fe07b866078870
Sylvia <sylviad1221@gmail.com>
2016-10-06 00:49:01 -0400

    add styling

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set("view engine", "ejs");
 var campgrounds = [
   {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg"},
   {name: "Camp Deer Park", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
+  {name: "Bear Mountain", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
+  {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg"},
+  {name: "Camp Deer Park", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
+  {name: "Bear Mountain", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"},
+  {name: "Salmon Creek", image: "https://farm3.staticflickr.com/2259/2182093741_164dc44a24.jpg"},
+  {name: "Camp Deer Park", image: "https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg"},
   {name: "Bear Mountain", image: "https://farm2.staticflickr.com/1281/4684194306_18ebcdb01c.jpg"}
 ];
 
