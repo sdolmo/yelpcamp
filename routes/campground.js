9e0788a8febd2b3ab5bb1be2c1f85778876b95b1
Sylvia <sylviad1221@gmail.com>
2017-01-24 05:32:06 -0500

    add author to campgrounds

--- routes/campground.js
+++ routes/campground.js
@@ -16,25 +16,30 @@ router.get("/", function(req, res){
 });
 
 // CREATE - Add new campgrounds to DB
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
   // get data from form and add to campgrounds array
   var name = req.body.name;
   var image = req.body.img;
   var desc = req.body.description;
-  var newCampground = {name: name, image: image, description: desc};
+  var author = {
+    id: req.user._id,
+    username: req.user.username
+  };
+  var newCampground = {name: name, image: image, description: desc, author: author};
   // Create a new campground and save to DB
   Campground.create(newCampground, function(err, newCamp) {
     if (err) {
       console.log(err);
     } else {
       // redirect back to campgrounds page
+      console.log(newCamp);
       res.redirect("/campgrounds")
     }
   })
 });
 
 // NEW - Display the form to make a new campground
-router.get("/new", function(req, res){
+router.get("/new", isLoggedIn, function(req, res){
   res.render("campgrounds/new")
 });
 
@@ -50,5 +55,14 @@ router.get("/:id", function(req, res){
     }
   })
 });
+
+// middleware
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
 
 module.exports = router;
